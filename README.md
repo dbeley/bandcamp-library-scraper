ce34e6b82a75e322f73741f0555532d8e83d91a0
dbeley <6568955+dbeley@users.noreply.github.com>
2022-12-06 21:04:35 +0100

    Add support for collection/artists/full_discography extraction.

--- README.md
+++ README.md
@@ -1,27 +1,60 @@
 # bandcamp-wishlist-scraper
 
-Export your bandcamp wishlist into a csv file.
+Export your bandcamp data into a csv file.
 
 This script does not download audio files, only metadata.
 
-Dependencies:
+It can extract any of those data:
+
+- collection (all the albums you own)
+- wishlist (all the albums in your wishlist)
+- artists (basic information about the artists you follow)
+- full_discography (all the albums from all the artists you follow - might take a long time to extract)
+
+# Usage
+
+- Go to your bandcamp profile in your web browser (i.e. "https://bandcamp.com/dbeley/wishlist")
+- Go to the tab you want to extract data from
+- Click on "see more" and scroll down until all the elements are loaded
+- Save the page in html in the same folder as this script
+
+Note: you can load all the different tabs at once in order to have all the information on the same file
+
+```
+python bandcamp-wishlist-scraper.py -f bandcamp-wishlist-export.html -t collection
+python bandcamp-wishlist-scraper.py -f bandcamp-wishlist-export.html -t wishlist
+python bandcamp-wishlist-scraper.py -f bandcamp-wishlist-export.html -t artists
+python bandcamp-wishlist-scraper.py -f bandcamp-wishlist-export.html -t full_discography
+```
+
+## Dependencies
+
+- python
 - requests
 - bs4
 - lxml
 
-Fields extracted:
+## Installation
+
+```
+git clone https://github.com/dbeley/bandcamp-wishlist-scraper
+cd bandcamp-wishlist-scraper
+pip install -r requirements.txt
+python bandcamp-wishlist-scraper.py -h
+```
+
+## Fields extracted
+
+### collection / wishlist / full_discography
+
 - artist name
 - album name
-- number of collections in
-- price (for the digital release)
+- number of collections in (only for wishlist and collection)
+- price for the digital release (only for wishlist and full_discography)
+- currency for the digital release (only for wishlist and full_discography)
 
-# Usage
+### artists
 
-- Go to your wishlist in your web browser (i.e. "https://bandcamp.com/dbeley/wishlist")
-- Go to your wishlist tab
-- Click on "see more" and scroll down until all the elements are loaded
-- Save the page in html in the same folder as this script
-
-```
-python bandcamp-wishlist-scraper.py -f bandcamp-wishlist-export.html
-```
+- artist name
+- url
+- location
--- bandcamp-wishlist-scraper.py
+++ bandcamp-wishlist-scraper.py
@@ -7,8 +7,8 @@ import logging
 logger = logging.getLogger()
 
 
-def read_from_fs(file_name: str):
-    with open(file_name, "r") as f:
+def read_soup_from_fs(filename: str):
+    with open(filename, "r") as f:
         content = f.read()
     return BeautifulSoup(content, "html.parser")
 
@@ -17,7 +17,7 @@ def get_soup(url):
     return BeautifulSoup(requests.get(url).content, "lxml")
 
 
-def extract_collection_infos(soup):
+def extract_wishlist(soup):
     list_albums = []
     for item in (
         soup.find("div", {"id": "wishlist-grid"})
@@ -53,6 +53,50 @@ def extract_collection_infos(soup):
     return list_albums
 
 
+def extract_following(soup):
+    list_artists = []
+    for item in (
+        soup.find("div", {"id": "following-bands-container"})
+        .find("ol")
+        .find_all("li", {"class": "following"})
+    ):
+        artist_infos = item.find("div", {"class": "fan-info-inner"})
+        list_artists.append(
+            {
+                "name": artist_infos.find("a", {"class": "fan-username"}).text,
+                "url": artist_infos.find("a", {"class": "fan-username"})["href"],
+                "location": artist_infos.find("div", {"class": "fan-location"}).text,
+            }
+        )
+    return list_artists
+
+
+def extract_collection(soup):
+    list_albums = []
+    for item in (
+        soup.find("div", {"id": "collection-items"})
+        .find("ol", {"class": "collection-grid"})
+        .find_all("li", {"class": "collection-item-container"})
+    ):
+        album_infos = item.find("div", {"class": "collection-title-details"})
+        list_albums.append(
+            {
+                "artist": album_infos.find("div", {"class": "collection-item-title"})
+                .text.strip()
+                .split("\n")[0],
+                "name": album_infos.find(
+                    "div", {"class": "collection-item-artist"}
+                ).text.strip()[4:],
+                "url": album_infos.find("a", {"class": "item-link"})["href"],
+                "collected": item.find("div", {"class": "collected-by-header"})
+                .find("a")
+                .text.strip()
+                .split(" ")[0],
+            }
+        )
+    return list_albums
+
+
 def extract_album_infos(album):
     soup_album = get_soup(album["url"])
     digital_element = soup_album.find("li", {"class": "buyItem digital"})
@@ -71,27 +115,88 @@ def extract_album_infos(album):
     return album
 
 
-def export_to_csv(list_items, file_name: str = "Export_bandcamp.csv"):
-    with open(file_name, "w") as f:
+def extract_discography(artist):
+    soup_artist = get_soup(artist["url"] + "music")
+    list_albums = []
+    try:
+        for item in (
+            soup_artist.find("div", {"class": "leftMiddleColumns"})
+            .find("ol", {"id": "music-grid"})
+            .find_all("li", {"class": "music-grid-item"})
+        ):
+            list_albums.append(
+                {
+                    "artist": artist["name"],
+                    "name": item.find("p", {"class": "title"}).text.strip(),
+                    "url": artist["url"] + item.find("a")["href"],
+                }
+            )
+        return [extract_album_infos(album) for album in list_albums]
+    except Exception as e:
+        logger.warning(
+            f"Couldn't extract informations for artist {artist['name']}: {e}"
+        )
+
+
+def export_albums_to_csv(list_items, filename: str):
+    with open(filename, "w") as f:
         f.write("artist;name;url;collected;price;currency;\n")
         for i in list_items:
             f.write(
-                f"{i.get('artist')};{i.get('name')};{i.get('url')};{i.get('collected')};{i.get('price')};{i.get('currency')};\n"
+                f'"{i.get("artist")}";"{i.get("name")}";{i.get("url")};{i.get("collected")};{i.get("price")};{i.get("currency")};\n'
             )
+    logger.debug("Export finished successfully.")
+
+
+def export_artists_to_csv(list_items, filename: str):
+    with open(filename, "w") as f:
+        f.write("name;url;location;\n")
+        for i in list_items:
+            f.write(f"{i.get('name')};{i.get('url')};{i.get('location')};\n")
     logger.debug("Export finished successfully.")
 
 
 def main():
     args = parse_args()
-    export_file_name = f"Export_bandcamp_{int(time.time())}.csv"
-    soup = read_from_fs(args.file)
-    list_albums = extract_collection_infos(soup)
-    logger.info(f"Extracting infos for {len(list_albums)} albums.")
+    soup = read_soup_from_fs(args.file)
+    if args.type == "wishlist":
+        list_wishlist = extract_wishlist(soup)
+        logger.info(f"Extracting infos for {len(list_wishlist)} albums in wishlist.")
 
-    list_albums_with_price = [extract_album_infos(album) for album in list_albums]
-    logger.debug(list_albums_with_price)
+        list_wishlist_with_price = [
+            extract_album_infos(album) for album in list_wishlist
+        ]
+        logger.debug(list_wishlist_with_price)
 
-    export_to_csv(list_albums_with_price, export_file_name)
+        export_filename = f"Export_bandcamp_wishlist_{int(time.time())}.csv"
+        export_albums_to_csv(list_wishlist_with_price, export_filename)
+    elif args.type == "artists":
+        list_artists = extract_following(soup)
+        logger.info(f"Extracting infos for {len(list_artists)} followed artists.")
+        export_filename = f"Export_bandcamp_artists_{int(time.time())}.csv"
+        export_artists_to_csv(list_artists, export_filename)
+        breakpoint()
+    elif args.type == "full_discography":
+        list_artists = extract_following(soup)
+        logger.info(
+            f"Extracting discography infos for {len(list_artists)} followed artists."
+        )
+        export_filename = f"Export_bandcamp_full_discography_{int(time.time())}.csv"
+
+        list_albums_with_price = [
+            extract_discography(artist) for artist in list_artists
+        ]
+        export_albums_to_csv(list_albums_with_price, export_filename)
+        breakpoint()
+    elif args.type == "collection":
+        list_albums = extract_collection(soup)
+        logger.info(f"Extracting infos for {len(list_albums)} albums in collection.")
+        export_filename = f"Export_bandcamp_collection_{int(time.time())}.csv"
+        export_albums_to_csv(list_albums, export_filename)
+    else:
+        logger.warning(
+            f"Type {args.type} is not supported. Choose one of wishlist, artists, full_discography or collection."
+        )
 
 
 def parse_args():
@@ -111,6 +216,14 @@ def parse_args():
         "-f",
         help="File containing bandcamp html data.",
         type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--type",
+        "-t",
+        help="Type of data to extract (choices: wishlist, artists, collection, full_discography. default: wishlist)",
+        type=str,
+        default="wishlist",
     )
     args = parser.parse_args()
 
